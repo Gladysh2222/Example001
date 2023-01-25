439f7fdddd6cfde6e5ace98a52796d137b762412
Oleg GLadyshev <oleg_gladysh@mail.ru>
2023-01-25 22:05:32 +0400

    Example005_ConditionElse

---
+++ Example005_ConditionElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША");
+}
+else
+{
+    мConsole.Write("Привет, ");
+    Console.WriteLine(username);
+}
