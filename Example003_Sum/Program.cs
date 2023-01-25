a257b90054b46a6922f203072f366e72ab5ede8b
Oleg GLadyshev <oleg_gladysh@mail.ru>
2023-01-25 22:14:50 +0400

    Example 006 base

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int NumberA = 3;
-int NumberB = 5;
+﻿int NumberA = new Random().Next (1, 10); // 1 2 3 4 ... 9
+Console.WriteLine(NumberA);
+int NumberB = new Random().Next(1, 10);
+Console.WriteLine(NumberB);
 int result = NumberA + NumberB;
 Console.WriteLine(result);
